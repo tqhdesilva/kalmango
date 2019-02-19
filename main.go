27bb6941b6ba039f1c38e756beaafa437e5a93c6
tqhdesilva <tdesilvaqihao@gmail.com>
2019-02-19 11:18:40 -0700

    added random seed

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-import "time"
+import (
+	"math/rand"
+	"time"
+)
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	screen := NewScreen(10, 10)
 	c := make(chan time.Time)
 	go screen.Run(.1, c)
+
 }
