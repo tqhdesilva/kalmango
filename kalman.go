9dbb80dd8c218473b31c4c5634a74f5964347e52
tqhdesilva <tdesilvaqihao@gmail.com>
2019-02-23 20:38:25 -0700

    shortened variable naming in kalman.go

--- kalman.go
+++ kalman.go
@@ -7,7 +7,6 @@ import (
 	"errors"
 
 	"gonum.org/v1/gonum/mat"
-	// "gonum.org/v1/gonum/floats"
 )
 
 type CovMat struct {
@@ -27,14 +26,14 @@ func (cm *CovMat) FromDense(d *mat.Dense) error {
 		return errors.New("can't convert non-symmetric Dense matrix to SymDense")
 	}
 	n, _ := d.Dims()
-	rowColumnView := make([]float64, n*n)
+	rcv := make([]float64, n*n)
 	for i := 0; i < n; i++ {
 		row := d.RawRowView(i)
 		for j, k := range row {
-			rowColumnView[i*n+j] = k
+			rcv[i*n+j] = k
 		}
 	}
-	cm.SymDense = mat.NewSymDense(n, rowColumnView)
+	cm.SymDense = mat.NewSymDense(n, rcv)
 	return nil
 }
 
@@ -56,17 +55,9 @@ type KalmanFilter struct {
 }
 
 func (k *KalmanFilter) Predict() error {
-	//var newMean *mat.VecDense
 	n := k.State.mean.Len()
-	newMean := mat.NewVecDense(n, make([]float64, n))
-	newCovDense := mat.NewDense(n, n, make([]float64, n*n))
-	covMat, err := NewCovMat(n, make([]float64, n*n))
-	if err != nil {
-		return err
-	}
-	newState := &State{
-		covariance: covMat,
-	}
+	x := mat.NewVecDense(n, make([]float64, n))
+	pd := mat.NewDense(n, n, make([]float64, n*n))
 	_, cols := k.prediction.Dims()
 	if cols != k.State.mean.Len() {
 		return errors.New("incorrect dimensions")
@@ -80,63 +71,65 @@ func (k *KalmanFilter) Predict() error {
 	if k.State.mean == nil {
 		return errors.New("state mean is nil")
 	}
-	newMean.MulVec(k.prediction, k.State.mean)
-	newCovDense.Mul(k.prediction, k.State.covariance)
-	newCovDense.Mul(newCovDense, k.prediction.T())
+	x.MulVec(k.prediction, k.State.mean)
+	pd.Mul(k.prediction, k.State.covariance)
+	pd.Mul(pd, k.prediction.T())
 
-	newState.mean = newMean
-	newCovMat, err := NewCovMat(n, make([]float64, n*n))
+	p, err := NewCovMat(n, make([]float64, n*n))
 	if err != nil {
 		return err
 	}
-	err = newCovMat.FromDense(newCovDense)
+	s := &State{
+		covariance: p,
+		mean:       x,
+	}
+	err = p.FromDense(pd)
 	if err != nil {
 		return err
 	}
-	newState.covariance = newCovMat
-	*k.State = *newState
+	s.covariance = p
+	*k.State = *s
 	return nil
 }
 
 func (k *KalmanFilter) Update(measurement *mat.VecDense) error {
 	// calculate K'
-	// (19)
 	n := k.State.mean.Len()
-	newKalmanGain := mat.NewDense(n, n, make([]float64, n*n))
-	newKalmanGain.Mul(k.stateToSensor, k.State.covariance)
-	newKalmanGain.Mul(newKalmanGain, k.stateToSensor.T())
-	newKalmanGain.Add(newKalmanGain, k.Sensor.covariance)
-	newKalmanGain.Inverse(newKalmanGain)
-	newKalmanGain.Mul(k.stateToSensor.T(), newKalmanGain)
-	newKalmanGain.Mul(k.State.covariance, newKalmanGain)
+	kg := mat.NewDense(n, n, make([]float64, n*n))
+	kg.Mul(k.stateToSensor, k.State.covariance)
+	kg.Mul(kg, k.stateToSensor.T())
+	kg.Add(kg, k.Sensor.covariance)
+	kg.Inverse(kg)
+	kg.Mul(k.stateToSensor.T(), kg)
+	kg.Mul(k.State.covariance, kg)
 
 	// calculate hat x'_k
-	newStateMean := mat.NewVecDense(n, make([]float64, n))
-	newStateMean.MulVec(k.stateToSensor, k.State.mean)
-	newStateMean.SubVec(measurement, newStateMean)
-	newStateMean.MulVec(newKalmanGain, newStateMean)
-	newStateMean.AddVec(k.State.mean, newStateMean)
+	x := mat.NewVecDense(n, make([]float64, n))
+	x.MulVec(k.stateToSensor, k.State.mean)
+	x.SubVec(measurement, x)
+	x.MulVec(kg, x)
+	x.AddVec(k.State.mean, x)
 
-	newStateCovarianceDense := mat.NewDense(n, n, make([]float64, n*n))
-	newStateCovarianceDense.Mul(k.stateToSensor, k.State.covariance)
-	newStateCovarianceDense.Mul(newKalmanGain, newStateCovarianceDense)
-	newStateCovarianceDense.Sub(k.State.covariance, newStateCovarianceDense)
-	newStateCovariance, err := NewCovMat(n, make([]float64, n*n))
+	// calculate p_k
+	pd := mat.NewDense(n, n, make([]float64, n*n))
+	pd.Mul(k.stateToSensor, k.State.covariance)
+	pd.Mul(kg, pd)
+	pd.Sub(k.State.covariance, pd)
+	p, err := NewCovMat(n, make([]float64, n*n))
 	if err != nil {
 		return err
 	}
-	// error definitely in FromDense
-	err = newStateCovariance.FromDense(newStateCovarianceDense)
+	err = p.FromDense(pd)
 	if err != nil {
 		return err
 	}
 
-	k.State = &State{newStateCovariance, newStateMean}
+	k.State = &State{p, x}
 	return nil
 }
 
-func NewKalmanFilter(initialMeasurement *mat.VecDense, timedelta float64) (*KalmanFilter, error) {
-	sensorCovMat, err := NewCovMat(4, []float64{
+func NewKalmanFilter(z *mat.VecDense, td float64) (*KalmanFilter, error) {
+	r, err := NewCovMat(4, []float64{
 		1.0, 0.0, 0.0, 0.0,
 		0.0, 1.0, 0.0, 0.0,
 		0.0, 0.0, 1.0, 0.0,
@@ -145,30 +138,28 @@ func NewKalmanFilter(initialMeasurement *mat.VecDense, timedelta float64) (*Kalm
 	if err != nil {
 		return nil, err
 	}
-	sensor := &Sensor{
-		sensorCovMat,
-	}
+	sensor := &Sensor{r}
 
-	stateToSensor := mat.NewDense(4, 4, []float64{
+	h := mat.NewDense(4, 4, []float64{
 		1.0, 0.0, 0.0, 0.0,
 		0.0, 1.0, 0.0, 0.0,
 		0.0, 0.0, 1.0, 0.0,
 		0.0, 0.0, 0.0, 1.0,
 	})
-	noise, err := NewCovMat(4, []float64{
+	q, err := NewCovMat(4, []float64{
 		0.0, 0.0, 0.0, 0.0,
 		0.0, 0.0, 0.0, 0.0,
 		0.0, 0.0, 0.0, 0.0,
 		0.0, 0.0, 0.0, 0.0,
 	})
-	prediction := mat.NewDense(4, 4, []float64{
-		1.0, 0.0, timedelta, 0.0,
-		0.0, 1.0, 0.0, timedelta,
+	f := mat.NewDense(4, 4, []float64{
+		1.0, 0.0, td, 0.0,
+		0.0, 1.0, 0.0, td,
 		0.0, 0.0, 1.0, 0.0,
 		0.0, 0.0, 0.0, 1.0,
 	})
-	// initially ought to be the same as sensorCovMat
-	stateCovMat, err := NewCovMat(4, []float64{
+	// initially ought to be the same as r
+	p, err := NewCovMat(4, []float64{
 		1.0, 0.0, 0.0, 0.0,
 		0.0, 1.0, 0.0, 0.0,
 		0.0, 0.0, 1.0, 0.0,
@@ -178,17 +169,17 @@ func NewKalmanFilter(initialMeasurement *mat.VecDense, timedelta float64) (*Kalm
 		return nil, err
 	}
 
-	initialState := &State{
-		stateCovMat,
-		initialMeasurement,
+	s := &State{
+		covariance: p,
+		mean:       z,
 	}
 
 	kf := &KalmanFilter{
-		sensor,
-		initialState,
-		stateToSensor,
-		noise,
-		prediction,
+		Sensor:        sensor,
+		State:         s,
+		stateToSensor: h,
+		noise:         q,
+		prediction:    f,
 	}
 	return kf, nil
 }
