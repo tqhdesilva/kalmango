4c2ff07614f7267f9c85d8308e822ccbbc53ef38
tqhdesilva <tdesilvaqihao@gmail.com>
2019-02-26 22:16:01 -0700

    added svg transition

--- assets/kalman.js
+++ assets/kalman.js
@@ -4,7 +4,6 @@ var svg = d3.select("#graph").append("svg").attr("height", 500).attr("width", 50
 var circle = svg.append("circle").attr("cx", 250).attr("cy", 250).attr("fill", "black").attr("r", 10)
 socket.addEventListener("message", function(event){
     var d = JSON.parse(event.data)
-    circle.attr("cx", 50 * d.actual_position[0])
-    circle.attr("cy", 50 * d.actual_position[1])
+    circle.transition().attr("cx", 50 * d.actual_position[0]).attr("cy", 50 * d.actual_position[1])
 })
 // socket.send("data")
